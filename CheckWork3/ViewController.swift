d1a20e21b86efa0b9adff7c858ea3b89ad21265b
mio <mio.rabbit971.snow@gmail.com>
2020-04-03 00:32:01 +0900

    Initial Commit

---
+++ CheckWork3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckWork3
+//
+//  Created by 原田澪 on 2020/04/03.
+//  Copyright © 2020 原田澪. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
